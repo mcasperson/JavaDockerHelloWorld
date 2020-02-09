5203bc1ce3f550684460bddecf0d9fe98f68bcd7
Matthew Casperson <matthewcasperson@gmail.com>
2020-02-09 12:03:51 +1000

    sample project created

---
+++ src/main/java/com/matthewcasperson/javadockerhelloworld/JavadockerhelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.matthewcasperson.javadockerhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadockerhelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadockerhelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthewcasperson/javadockerhelloworld/JavadockerhelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewcasperson.javadockerhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadockerhelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
